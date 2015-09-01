594a60d3383020445c55af483ee8cc8be66d5d32
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-01 11:22:57 -0400

    created list controller & view is working

--- www/js/app.js
+++ www/js/app.js
@@ -3,9 +3,43 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
+// Store the angular.module in app
+var app = angular.module('starter', ['ionic']);
+
+// Controller for listing notes
+app.controller('ListCtrl', function($scope) {
+  // list of notes
+  $scope.notes = [
+    {
+      title: 'Note Title 1',
+      author: 'Marcus',
+      description: 'This is the description for Note 1'
+    },
+    {
+      title: 'Note Title 2',
+      description: 'This is the description for Note 2'
+    },
+    {
+      title: 'Note Title 3',
+      description: 'This is the description for Note 3'
+    },
+    {
+      title: 'Note Title 4',
+      description: 'This is the description for Note 4'
+    },
+    {
+      title: 'Note Title 5',
+      description: 'This is the description for Note 5'
+    },
+    {
+      title: 'Note Title 6',
+      description: 'This is the description for Note 6'
+    }
+  ];
+});
+
+app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
@@ -16,4 +50,4 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
